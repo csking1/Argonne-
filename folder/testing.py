b0dea8d6d5565d00262249f8941cb1fb0240124d
csking1 <csking1@uchicago.edu>
2016-07-22 11:08:19 -0500

    move sample class

---
+++ folder/testing.py
@@ -0,0 +1,15 @@
+
+def class Cats(object):
+    """A sample class for encompassing all things cat-like for feline lovers such as myself """
+    def __init__(self, breed, name, weight, eye_color, body_type):
+        self.name = name
+        self.breed = breed
+        self.weight = weight
+        self.eye_color = eye_color
+        self.bodytype = body_type
+
+    def health (self):
+        pass
+
+    def eating_habits(self):
+        pass
