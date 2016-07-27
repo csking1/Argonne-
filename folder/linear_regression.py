1486bea34ef29e336a6238db774af582e1d41f7d
Charity Susan King <csking1@mit.edu>
2016-07-27 16:38:47 -0500

    Adding graphic

--- folder/linear_regression.py
+++ folder/linear_regression.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-# style.use('gggplot')
+style.use('ggplot')
 
 def get_data():
 	df = quandl.get('WIKI/GOOGL')
@@ -27,7 +27,7 @@ def generate_features():
 	##shifting columns negatively
 	df['label'] = df[forecast_col].shift(-forecast_out)
 	return df, forecast_out
-def get_x_and_y():
+def train_test_predict():
 	df, forecast_out= generate_features()
 	X = np.array(df.drop(['label'], 1))
 	X = preprocessing.scale(X)
@@ -36,17 +36,33 @@ def get_x_and_y():
 	y = np.array(df['label'])
 	X_lately = X[-forecast_out:]
 	y=np.array(df['label'])
-	print(len(X), len(y))
 	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 	# clf = LinearRegression(n_jobs=1)
 	clf=svm.SVR()
 	clf.fit(X_train, y_train)
 	accuracy = clf.score(X_test, y_test)
-	print ("the accuracy score is {}".format(accuracy))
-	##LR is approximately 0.96
-	##SVM is approximately 0.79
-
+	# print ("the accuracy score is {}".format(accuracy))
+	##LR is approximately 0.96, SVM is approximately 0.79
 	#now, predict based on X data. 
-	# forecast_set=clf.predict(X_lately)
+	forecast_set=clf.predict(X_lately)
 	# print(forecast_set, accuracy, forecast_out)
-get_x_and_y()
+	##########################Graph##############################
+	df['Forecast'] = np.nan
+	last_date=df.iloc[-1].name
+	last_unix=last_date.timestamp()
+	one_day=86400
+	next_unix=last_unix + one_day
+	for i in forecast_set:
+		next_date=datetime.datetime.fromtimestamp(next_unix)
+		next_unix+=86400
+		df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+	df['Adj. Close'].plot()
+	df['Forecast'].plot()
+	plt.legend(loc=4)
+	plt.xlabel('Date')
+	plt.ylabel('Price')
+	plt.savefig('fig.png')
+	# plt.show()
+train_test_predict()
+
